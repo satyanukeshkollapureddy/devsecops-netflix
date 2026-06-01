29296dfc50de238044238d48e103258ce71530da
satyanukeshkollapureddy <satyanukeshkollapureddy@gmail.com>
2026-06-01 13:16:33 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "devsecops-netflix-reyaz-16" # Replace with your actual S3 bucket name
+    bucket = "sandy-netflix" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 }
